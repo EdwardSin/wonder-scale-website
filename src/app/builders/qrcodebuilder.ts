81445f8296087c28c6b4895e8e142502d0c9a4da
Edward Sin <edwardsin99@hotmail.com>
2020-06-05 22:54:14 +0800

    .: QR code image slow render issue (WSW-25)

--- src/app/builders/qrcodebuilder.ts
+++ src/app/builders/qrcodebuilder.ts
@@ -2,22 +2,28 @@ import { environment } from '@environments/environment';
 
 declare var jQuery: any;
 
-export class QRCodeBuilder{
+export class QRCodeBuilder {
 
     static URL = environment.URL;
-    public static createQRcode(target, username, id, option = {}){
-        let code = QRCodeBuilder.URL + username + '?id=' + id;
-        (<any>jQuery(target)).qrcode({width: option['width'] || 196, height: option['height']|| 196, foreground:"#000", 
-        correctLevel: 0, 
-        text: code, 
-        src: 'assets/images/png/icon-with-profile-image-borderless.png'
-    })
+    public static createQRcode(target, username, option = {}) {
+        let code = QRCodeBuilder.URL + 'shop/' + username;
+        return new Promise((resolve) => {
+            (<any>jQuery(target)).qrcode({
+                width: option['width'] || 196, height: option['height'] || 196, foreground: "#000",
+                correctLevel: 0,
+                text: code,
+                src: 'assets/images/png/icon-with-profile-image-borderless.png'
+            });
+            resolve();
+        })
     }
-    public static createPromotionQrCode(target, url, option = {}){
+    public static createPromotionQrCode(target, url, option = {}) {
         let code = QRCodeBuilder.URL + url;
-        (<any>jQuery(target)).qrcode({width: option['width'] || 196, height: option['height']|| 196, foreground:"#000", 
-        correctLevel: 0, 
-        text: code, 
-        src: 'assets/images/png/icon-with-profile-image-borderless.png'})
+        (<any>jQuery(target)).qrcode({
+            width: option['width'] || 196, height: option['height'] || 196, foreground: "#000",
+            correctLevel: 0,
+            text: code,
+            src: 'assets/images/png/icon-with-profile-image-borderless.png'
+        })
     }
 }
