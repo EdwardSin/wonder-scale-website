9b18d10860b8f8fbc4077b91d77517aae13b95ad
Edward Sin <edwardsin99@hotmail.com>
2020-06-24 20:51:04 +0800

    .: Change URL from "shop" to "page" (WSW-61)

--- src/app/elements/float-banner/float-banner.component.ts
+++ src/app/elements/float-banner/float-banner.component.ts
@@ -58,7 +58,7 @@ export class FloatBannerComponent implements OnInit {
     if (changes && this.element) {
       if (this.type == 'shop') {
         this.isFollowedShop();
-        this.link = environment.URL + 'shop/' + this.element.username + '?id=' + this.element._id;
+        this.link = environment.URL + 'page/' + this.element.username + '?id=' + this.element._id;
         this.shareLinkThroughFB = this.link;
         this.shareLinkThroughTwitter = 'https://twitter.com/intent/tweet?text=Welcome to view my page now. ' + this.link;
         this.shareLinkThroughEmail = 'mailto:?body=' + this.link;
