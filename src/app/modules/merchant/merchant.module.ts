1e15d7d7ff9db91cf80a192359412ba44164f704
Edward Sin <edwardsin99@hotmail.com>
2020-10-18 14:33:01 +0800

    .: CLONE - Store Page (WSW-81)

--- src/app/modules/merchant/merchant.module.ts
+++ src/app/modules/merchant/merchant.module.ts
@@ -3,12 +3,14 @@ import { CommonModule } from '@angular/common';
 
 import { MerchantRoutingModule } from './merchant-routing.module';
 import { MerchantComponent } from '@components/merchant/merchant.component';
-import { SharedModule } from '../public/shared/shared.module';
 import { SharedMerchantModule } from '../public/shared-merchant/shared-merchant.module';
+import { SharedModule } from '../public/shared/shared.module';
+import { MerchantPageComponent } from '@elements/merchant-page/merchant-page.component';
 
 
 @NgModule({
-  declarations: [MerchantComponent],
+  declarations: [MerchantComponent,
+    MerchantPageComponent],
   imports: [
     CommonModule,
     SharedModule,
